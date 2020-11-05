9984a86a358bbc9ce6e374e57bdfd622dd5e6e17
varadsant <33028830+varadsant@users.noreply.github.com>
2020-11-05 01:41:11 +0000

    Testing App

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent {
+	public num1:number;
+	public num2:number;
+	public result:number;
+	public resultheading:any;
+
+	sum(){
+		this.result = this.num1 + this.num2;
+		this.resultheading = this.num1 + ' + ' + this.num2; 
+	}
+	sub(){
+		this.result = this.num1 - this.num2;
+		this.resultheading = this.num1 + ' - ' + this.num2; 
+	}
+	mul(){
+		this.result = this.num1 * this.num2;
+		this.resultheading = this.num1 + ' x ' + this.num2; 
+	}
+	div(){
+		this.result = this.num1 / this.num2;
+		this.resultheading = this.num1 + ' / ' + this.num2; 
+	} 
+
+}
